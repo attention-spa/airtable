ab490babccfabe6ac92630b6d54a52fd3ecfe520
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-09-15 11:48:20 +0200

    Add clean remoteBase ESM entry

---
+++ utils/remote-base/index.ts
@@ -0,0 +1,25 @@
+export { createRemoteBase, remoteBase } from './lib/class.ts';
+export type {
+    AirtableRecordFields,
+    DeferredRemoteTable,
+    DeleteInput,
+    DeletedRecord,
+    RemoteBase,
+    RemoteBaseCallable,
+    RemoteBaseConfig,
+    RemoteBaseHandle,
+    RemoteBaseUpdate,
+    RemoteBaseUpdateOperation,
+    RemoteBaseUpdateResult,
+    RemoteFieldSchema,
+    RemoteRecord,
+    RemoteTable,
+    RemoteTableRegistry,
+    RemoteTableSchema,
+    UpdateRecordInput,
+    UpsertInput,
+    UpsertOptions,
+    UpsertResult,
+} from './lib/types.ts';
+
+export { remoteBase as default } from './lib/class.ts';
