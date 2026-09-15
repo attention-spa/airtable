1bfeb4102808202833d02b389264442d4cde29f3
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-09-15 09:50:46 +0200

    Export remoteBase utility

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,3 @@
 export { selectFullRecordsAsync } from "./selectFullRecordsAsync/index.ts";
 export { SmartBase } from "./smart-base/index.ts";
+export { default as remoteBase, createRemoteBase } from "./remote-base/index.js";
