262408d4674435edbeaaba84a08df5bdae2d0d3a
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-09-15 03:41:25 +0200

    docs: preserve legacy Container

---
+++ legacy/airtable-extension/main/src/components/Container.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Box } from "@airtable/blocks/ui";
+import { BoxProps } from "@airtable/blocks/dist/types/src/ui/box";
+
+
+
+export const RenderBox = (props: BoxProps) => <Box {...props} />;
