627ed68a8d6af503d89d386f77e853c8b6945780
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-09-15 03:41:10 +0200

    docs: preserve legacy CodeContainer

---
+++ legacy/airtable-extension/main/src/components/CodeContainer.tsx
@@ -0,0 +1,34 @@
+import React, { Attributes, ReactChildren, ReactElement } from "react"
+import { RenameProperty as Rename } from '../utils';
+
+export type CodeContainerProps = {
+    code: string,
+    /** Acts as the container's heading. */
+    summary?: string,
+    language?: string,
+    /** Set to true to have the code container open by default. */
+    open?: boolean
+};
+
+/** Renders a toggleable code container. */
+export const RenderCodeContainer = (props: CodeContainerProps | string): ReactElement => {
+
+    const {
+        code = typeof props === 'string' ? props : '',
+        summary = '',
+        language = '',
+        open
+    } = props as CodeContainerProps;
+
+    if (!code || typeof code !== 'string')
+        throw `#ERROR@RenderCodeContainer: Code must be a ${code === '' ? 'non-empty' : ''} string!`;
+
+    return React.createElement(
+        'details',
+        open === true ? { open } : {},
+        <summary>{summary}</summary>,
+        <br />,
+        ['```' + language, code, '```'].join('\n')
+    );
+
+};
