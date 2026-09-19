93641c66f731f485306e8b16601bb5dd4d8de2c7
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-09-19 22:41:47 +0200

    Expose shared sequential Airtable batching

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,13 @@
 export { selectFullRecordsAsync } from "./selectFullRecordsAsync/index.ts";
 export { SmartBase } from "./smart-base/index.ts";
 export { default as remoteBase, createRemoteBase } from "./remote-base/index.ts";
+export {
+  chunk,
+  runInBatches,
+  MutationLimiter,
+  OperationLimiter,
+  createReferenceIndex,
+  normalizeRef,
+  resolveReference,
+} from "./core/index.ts";
+export type { AirtableNamedReference } from "./core/index.ts";
