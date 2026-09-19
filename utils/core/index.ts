08f2c56b275a06a6ea979b1a5ca273920c36b96b
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-09-19 21:34:29 +0200

    Add shared Airtable core index.ts

---
+++ utils/core/index.ts
@@ -0,0 +1,8 @@
+export { chunk } from "./batching.ts";
+export {
+  createReferenceIndex,
+  normalizeRef,
+  resolveReference,
+} from "./reference.ts";
+export { MutationLimiter, OperationLimiter } from "./limiter.ts";
+export type { AirtableNamedReference } from "./reference.ts";
